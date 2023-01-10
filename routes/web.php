2df101ada1a10e34cb1a0ccaf649ab1bd6e187c0
ahjoel <aholoujoel5@gmail.com>
2023-01-10 11:00:20 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', \App\Http\Livewire\HomeComponent::class);
+
+Route::get('/shop', \App\Http\Livewire\ShopComponent::class);
+
+Route::get('/cart', \App\Http\Livewire\CartComponent::class);
+
+Route::get('/checkout', \App\Http\Livewire\CheckoutComponent::class);
